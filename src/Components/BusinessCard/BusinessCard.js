03583630e8395809f924bffaab13921f54af6918
Shawn Smith <sds.smith24@gmail.com>
2022-03-22 08:32:47 -0400

    cleans up BusinessCard.css, adds folders for Header, Footer, Contact

--- src/Components/BusinessCard/BusinessCard.js
+++ src/Components/BusinessCard/BusinessCard.js
@@ -2,17 +2,14 @@ import './BusinessCard.css'
 
 function BusinessCard() {
     return (
-        <section className='page' id='page-1'>
-            <video autoplay muted loop className="myVideo mobile">
-                <source src="../../media/Sea - desktop 24216 (1).mp4" type="video/mp4"/>
-            </video>
-            <video autoplay muted loop className="myVideo desktop">
+        <div className='BusinessCard' >
+            <video autoplay muted loop className="myVideo">
                 <source src="./media/Sea - mobile 24216.mp4" type="video/mp4"/>
             </video>
             <h1 className='full-width center name' id='name'>Shawn Smith</h1>
             <p className='hr'></p>
             <h4 className='full-width center role' id='role'>front-end web developer</h4>                
-        </section>
+        </div>
     );
   }
   
