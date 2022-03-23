044ec6e6d720a197e4750d7ccd60cc3b3720f664
Shawn Smith <sds.smith24@gmail.com>
2022-03-23 08:36:11 -0400

    adds profile pic and description in About, adds media query for mobile in About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,19 @@
 import './About.css'
+import Portrait from '../../media/portrait.jpeg'
 
 function About() {
     return (
         <div className='About' id='page-3'>
-            <h1 id='name'>Shawn Smith</h1>
-            <p className='hr'></p>
-            <h4  id='role'>front-end web developer</h4> 
-            <div className='about'>
+            <div className='profile'>
+                <img src={Portrait} />
+                <div className='name-title'>
+                    <h1 id='name'>Shawn Smith</h1>
+                    <p className='hr'></p>
+                    <h4  id='role'>front-end web developer</h4> 
+                </div>
+            </div>
+
+            <div className='about-section'>
                 <div className='about-title'>
                     <h3 >About:</h3>
 
@@ -14,12 +21,20 @@ function About() {
 
                 <div className='about-paragraph'>
 
-                    <p className='about'>Metamatopoeia Productions is a boutique web development studio based in Hanover, Pennsylvania.</p>
-                    <p className='about'>With capabilities in both front-end and back-end web development, as well as visual styling and design, we are eager to help you with all of your web needs.  Specialties include:</p>
+                    <p >After working most of my adult life in the restaurant industry, in 2021 I decided a career change was in order.</p>
+                    <p >With the support of my wife and family, I set myself down the path toward certification in Web Development.</p>
+                    <p >From my experience as a multi-unit restaurant leader, I bring soft skills like strong communication, collaboration, relationship-building.</p>
+                    <p >From my studies in Web Development, I have attained valuable technical skills, including:</p>
                     <ul>
                         <li>Responsive Web Design</li>
+                        <li>Dynamic Web Development and asyncronous coding</li>
                         <li>Javascript Algorithms and Data Structures</li>
                         <li>Front End Development Libraries</li>
+                            <ul>
+                                <li>React.js</li>
+                                <li>Vue</li>
+                            </ul>
+                        <li>Single-page Web Apps</li>
                         <li>Web Accessibility</li>
                         <li>Web APIs</li>
                     </ul>
