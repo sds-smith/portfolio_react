8113852683e7df6ff5bf2aad1148c561806dfee0
Shawn Smith <sds.smith24@gmail.com>
2022-03-23 14:35:01 -0400

    adds form inputs to ContactForm

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -8,7 +8,16 @@ function ContactForm() {
                 <h1 id='name'>Shawn Smith</h1>
                 <p className='hr'></p>
                 <h4 id='role'>contact</h4>
-            </div>              
+                <form className='form' >
+                    <input type='text' placeholder='Your First Name' required />
+                    <input type='text' placeholder='Your Last Name' required />
+                    <input type='email' placeholder='Your Email Address' required />
+                    <inpout type='tel' placeholder='Your Telephone Number' />
+                    <textarea placeholder='Comments' />
+                    <button type='submit'>SUBMIT</button>
+            </form> 
+            </div> 
+            
         </div>
     );
   }
