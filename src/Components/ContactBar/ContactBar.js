6030e54ccf2cde49b040eea9a08b85d5191f7672
Shawn Smith <sds.smith24@gmail.com>
2022-03-24 09:51:13 -0400

    adds rel='noreferrer' to  ContactBar target_blanks

--- src/Components/ContactBar/ContactBar.js
+++ src/Components/ContactBar/ContactBar.js
@@ -16,8 +16,8 @@ function ContactBar(props) {
         <div className='contact-info' id='contact'>
             <Link to='/contact_form' className='side-nav' onClick={toggleContact}>contact:</Link>
             <h5><a className='contact-link'href='mailto:sds.smith24@gmail.com' id='email'   onClick={toggleContact}>sds.smith24@gmail.com</a></h5>
-            <h5><a className='contact-link'href='https://www.linkedin.com/in/shawn-smith-3379ab13/' target='_blank'  onClick={toggleContact}>linkedin.com/shawn-smith</a></h5>
-            <h5><a className='contact-link'href='https://github.com/sds-smith' target='_blank' onClick={toggleContact}>github.com/sds-smith</a></h5>
+            <h5><a className='contact-link'href='https://www.linkedin.com/in/shawn-smith-3379ab13/' target='_blank'  rel="noreferrer" onClick={toggleContact}>linkedin.com/shawn-smith</a></h5>
+            <h5><a className='contact-link'href='https://github.com/sds-smith' target='_blank' rel="noreferrer" onClick={toggleContact}>github.com/sds-smith</a></h5>
             <h5><a className='contact-link'href='tel:12156305599'>215.630.5599</a></h5>                
         </div>
     );
