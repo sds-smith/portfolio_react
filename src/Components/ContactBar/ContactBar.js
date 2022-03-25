c05dd66d023caa5c861374f1a92dddcdf9b9b5d0
Shawn Smith <sds.smith24@gmail.com>
2022-03-25 10:20:55 -0400

    add contact icons for mobile

--- src/Components/ContactBar/ContactBar.js
+++ src/Components/ContactBar/ContactBar.js
@@ -1,5 +1,9 @@
 import './ContactBar.css'
 import { Link } from 'react-router-dom'
+import email_icon from '../../media/email_icon.png'
+import linkedin_icon from '../../media/linkedin_icon.png'
+import github_icon from '../../media/github_icon.png'
+import phone_icon from '../../media/phone_icon.png'
 
 function ContactBar(props) {
 
@@ -15,10 +19,10 @@ function ContactBar(props) {
     return (
         <div className='contact-info' id='contact'>
             <Link to='/contact_form' className='side-nav' onClick={toggleContact}>contact:</Link>
-            <h5><a className='contact-link'href='mailto:sds.smith24@gmail.com' id='email'   onClick={toggleContact}>sds.smith24@gmail.com</a></h5>
-            <h5><a className='contact-link'href='https://www.linkedin.com/in/shawn-smith-3379ab13/' target='_blank'  rel="noreferrer" onClick={toggleContact}>linkedin.com/shawn-smith</a></h5>
-            <h5><a className='contact-link'href='https://github.com/sds-smith' target='_blank' rel="noreferrer" onClick={toggleContact}>github.com/sds-smith</a></h5>
-            <h5><a className='contact-link'href='tel:12156305599'>215.630.5599</a></h5>                
+            <h5><a className='contact-link'href='mailto:sds.smith24@gmail.com' id='email'   onClick={toggleContact}><span>sds.smith24@gmail.com</span><img className='icon' src={email_icon} alt='email icon for sds.smith24@gmail.com'/></a></h5>
+            <h5><a className='contact-link'href='https://www.linkedin.com/in/shawn-smith-3379ab13/' target='_blank'  rel="noreferrer" onClick={toggleContact}><span>linkedin.com/shawn-smith</span><img className='icon' src={linkedin_icon} alt='linkedin icon for linkedin.com/shawn-smith'/></a></h5>
+            <h5><a className='contact-link'href='https://github.com/sds-smith' target='_blank' rel="noreferrer" onClick={toggleContact}><span>github.com/sds-smith</span><img className='icon' src={github_icon} alt='github icon for github.com/sds-smith'/></a></h5>
+            <h5><a className='contact-link'href='tel:12156305599'><span>215.630.5599</span><img className='icon' src={phone_icon} alt='phone icon for 215.630.5599'/></a></h5>                
         </div>
     );
   }
