8d0f611668d4b4e456ceecb3cff2be4eae1a7f28
Shawn Smith <sds.smith24@gmail.com>
2022-03-25 12:08:25 -0400

    fix video render

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,10 +2,11 @@
 import { useMediaQuery } from '../../util/customHooks'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
+import backgroundVideo from '../../media/Sea - desktop 24216 (1).mp4'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import ContactBar from '../ContactBar/ContactBar'
-import Background from '../Background/Background'
+// import Background from '../Background/Background'
 import BusinessCard from '../BusinessCard/BusinessCard';
 import Portfolio from '../Portfolio/Portfolio'
 import About from '../About/About'
@@ -17,40 +18,42 @@ function App() {
   let isMobile = useMediaQuery('(max-width: 1020px)')
 
   return (
-    <Router>
-      <Route >
-        <Header />
-      </Route>
+    
+      <Router>
+        <video src={backgroundVideo} autoPlay muted loop playsInline className='BackgroundVideo' />
+        <Route >
+          <Header />
+        </Route>
 
-      <Route>
-        <ContactBar isMobile={isMobile}/>
-      </Route>
+        <Route>
+          <ContactBar isMobile={isMobile}/>
+        </Route>
 
-      <Route>
-        <Background />
-      </Route>
-      
-      <Route exact path='/'>
-        <BusinessCard />
-      </Route>
+        <Route exact path='/'>
+          <BusinessCard />
+        </Route>
 
-      <Route path='/portfolio'>
-        <Portfolio />
-      </Route>
+        <Route path='/portfolio'>
+          <Portfolio />
+        </Route>
 
-      <Route path='/about'>
-        <About />
-      </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
 
-      <Route>
-        <Footer />
-      </Route>  
+        <Route>
+          <Footer />
+        </Route>  
 
-      <Route path='/contact_form'>
-        <ContactForm />
-      </Route>  
-    </Router>
+        <Route path='/contact_form'>
+          <ContactForm />
+        </Route>  
 
+        {/* <Route> */}
+          {/* <Background /> */}
+        {/* </Route> */}
+
+      </Router>
   );
 }
 
