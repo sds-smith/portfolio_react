accde7531711e40cf76f6c71dc66e0d45f48de39
Shawn Smith <sds.smith24@gmail.com>
2022-03-25 06:24:26 -0400

    connect ContactForm to form.html, style ContactForm

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -1,7 +1,41 @@
-
+import { useState } from 'react'
 import './ContactForm.css'
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
 function ContactForm() {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ 
+                        "form-name": "contact", 
+                        "first-name": firstName,
+                        "last-name": lastName,
+                        "email": email,
+                        "message": message
+                     })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+  
+        e.preventDefault();
+      };
+  
+    const handleChangeFirstName = e => setFirstName(e.target.value);
+    const handleChangeLastName = e => setLastName(e.target.value);
+    const handleChangeEmail = e => setEmail(e.target.value);
+    const handleChangeMessage = e => setMessage(e.target.value);
 
     return (
         <div className='ContactForm' >
@@ -9,12 +43,11 @@ function ContactForm() {
                 <h1 id='name'>Shawn Smith</h1>
                 <p className='hr'></p>
                 <h4 id='role'>contact</h4>
-                <form className='form' >
-                    <input type='text' placeholder='Your First Name' required />
-                    <input type='text' placeholder='Your Last Name' required />
-                    <input type='email' placeholder='Your Email Address' required />
-                    <inpout type='tel' placeholder='Your Telephone Number' />
-                    <textarea placeholder='Comments' />
+                <form className='form' onSubmit={handleSubmit} >
+                    <input type='text' placeholder='Your First Name' name='first-name' value={firstName} required onChange={handleChangeFirstName}/>
+                    <input type='text' placeholder='Your Last Name' name='last-name' value={lastName} required onChange={handleChangeLastName}/>
+                    <input type='email' placeholder='Your Email Address' name='email' value={email} required onChange={handleChangeEmail}/>
+                    <textarea placeholder='Comments' name='message' value={message} required onChange={handleChangeMessage}/>
                     <button type='submit'>SUBMIT</button>
                 </form> 
             </div> 
