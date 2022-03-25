2a7c8afb769581acafb0d70e8ef8db2c229ac076
Shawn Smith <sds.smith24@gmail.com>
2022-03-25 10:40:06 -0400

    adds state to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Header.css'
 
 function Header() {
 
+    const [navVisible, setNavVisible] = useState(false)
+    const [contactBar, setContactBar] = useState(false)
+
     const toggleNav = () => {
         let navBar = document.getElementById('NavBar')
         let hamburger = document.getElementById('hamburger')
-        console.log(navBar.style.display)
-        if (navBar.style.display === 'none') {
+        if (!navVisible) {
             navBar.style.display = 'flex'
             hamburger.innerHTML = 'x'
             hamburger.style.fontSize = '25px'
@@ -16,28 +19,30 @@ function Header() {
             hamburger.innerHTML = '&#9776;'
             hamburger.style.fontSize = '50px'
         }
+        setNavVisible(!navVisible)
     }
 
     const toggleContact = () => {
         const contact = document.getElementById('contact')
         const contactButton = document.getElementById('contactMobile')
-        if (contact.style.display === 'none') {
+        if (!contactBar) {
             contact.style.display = 'flex'
             contactButton.style.fontWeight = 'bold'
         } else {
             contact.style.display = 'none'
             contactButton.fontWeight = 'normal'
         }
+        setContactBar(!contactBar)
     }
 
     return (
         <div className='Header'>
             <div className='topnav' id='my-topnav'>
                 <div className='NavBar' id='NavBar'>
-                    <NavLink to='/' exact className='nav-items'>Home</NavLink>
-                    <NavLink to='/portfolio' className='nav-items'>Portfolio</NavLink>
-                    <NavLink to='/about' className='nav-items'>About</NavLink>  
-                    <button className='nav-items contactMobile' id='contactMobile' onClick={toggleContact}>Contact</button> 
+                    <NavLink to='/' exact className='nav-items'activeClassName='active' >Home</NavLink>
+                    <NavLink to='/portfolio' className='nav-items'activeClassName='active' >Portfolio</NavLink>
+                    <NavLink to='/about' className='nav-items'activeClassName='active' >About</NavLink>  
+                    <button className='nav-items contactMobile' activeClassName='active' id='contactMobile' onClick={toggleContact}>Contact</button> 
                 </div>      
                 <button className="hamburger" id='hamburger' onClick={toggleNav}>&#9776;</button>          
             </div>
