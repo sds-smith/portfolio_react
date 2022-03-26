89ed3e1647990550d2fd7015c4b5849b8cee25dd
Shawn Smith <sds.smith24@gmail.com>
2022-03-25 20:54:17 -0400

    adds link to readme in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,11 @@ import './Footer.css'
 function Footer() {
     return (
         <footer>
-            <h4><span>&copy;</span> 2021 Metamatopoeia Productions</h4>
+            <div className='footer-content'>
+                <h4 className='copyright'><span>&copy;</span> 2022 Metamatopoeia</h4>
+                <h4><a href='https://github.com/sds-smith/portfolio_react#readme' target='_blank'  rel="noreferrer" >Site Details</a></h4>
+            </div>
+
         </footer>
     );
   }
