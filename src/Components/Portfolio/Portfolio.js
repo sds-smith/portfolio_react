3cf139db4a157c8fa53b083855cbb87bec922665
Shawn Smith <sds.smith24@gmail.com>
2022-03-30 21:17:10 -0400

    add portfolio cards and begin to style

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -10,16 +10,22 @@ function Portfolio() {
                 <h4 id='role'>portfolio samples</h4>
                 <ul className='portfolio-list'>
                     <li>
-                        <Link className='portfolio-link' id='atj' to={{ pathname: "https://assemblethejams.netlify.app" }} target="_blank" >Assemble<span>the</span>Jams</Link>
-                        <span className='divider'>|</span>
-                        <Link className='portfolio-link rm' id='atj-readme' to={{ pathname: "https://github.com/sds-smith/assemble-the-jams#readme"}} target='_blank'>(README.md)</Link>  
-                        <p className='description'>Connect to Spotify API, search songs, create custom playlist, and save playlist to your Spotify profile.</p>
+                        <div className='card' id='atj-card'>
+                            {/* <Link className='portfolio-link' id='atj' to={{ pathname: "https://assemblethejams.netlify.app" }} target="_blank" >Assemble<span>the</span>Jams</Link> */}
+                            {/* <span className='divider'>|</span> */}
+                            {/* <Link className='portfolio-link rm' id='atj-readme' to={{ pathname: "https://github.com/sds-smith/assemble-the-jams#readme"}} target='_blank'>(README.md)</Link>   */}
+                            {/* <p className='description'>Connect to Spotify API, search songs, create custom playlist, and save playlist to your Spotify profile.</p> */}
+                        </div>
+
                     </li><br/>   
                     <li>
-                        <Link className='portfolio-link' id='pwgen' to={{ pathname: "https://sds-smith.github.io/password_generator/" }} target="_blank" >Password Generator</Link>
-                        <span className='divider'>|</span>
-                        <Link className='portfolio-link rm' id='pwgen-readme' to={{ pathname: "https://github.com/sds-smith/password_generator#readme"}} target='_blank'>(README.md)</Link>  
-                        <p className='description'>Generate random unique password and copy to clipboard</p>
+                        <div className='card' id='rpg-card'>
+                            {/* <Link className='portfolio-link' id='pwgen' to={{ pathname: "https://sds-smith.github.io/password_generator/" }} target="_blank" >Password Generator</Link> */}
+                            {/* <span className='divider'>|</span> */}
+                            {/* <Link className='portfolio-link rm' id='pwgen-readme' to={{ pathname: "https://github.com/sds-smith/password_generator#readme"}} target='_blank'>(README.md)</Link>   */}
+                            {/* <p className='description'>Generate random unique password and copy to clipboard</p> */}
+                        </div>
+
                     </li> 
                 </ul> 
             </div>              
