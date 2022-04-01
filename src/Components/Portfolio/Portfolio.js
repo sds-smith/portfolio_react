e5574937d763517184387455cd4bf7d9b7d92558
Shawn Smith <sds.smith24@gmail.com>
2022-04-01 09:18:27 -0400

    format cards in mobile

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -4,31 +4,28 @@ import './Portfolio.css'
 function Portfolio() {
     return (
         <div className='Portfolio' id='page-2'>
-            <div className='centered-me'>
                 <h1 id='name'>Shawn Smith</h1>
                 <p className='hr'></p>
                 <h4 id='role'>portfolio samples</h4>
                 <ul className='portfolio-list'>
                     <li>
+                        <h3>Playlist Creator</h3>
                         <div className='card' id='atj-card'>
                             <Link className='portfolio-link' id='atj' to={{ pathname: "https://assemblethejams.netlify.app" }} target="_blank" >Demo</Link>
-                            {/* <span className='divider'>|</span> */}
                             <Link className='portfolio-link rm' id='atj-readme' to={{ pathname: "https://github.com/sds-smith/assemble-the-jams#readme"}} target='_blank'>About</Link>  
-                            {/* <p className='description'>Connect to Spotify API, search songs, create custom playlist, and save playlist to your Spotify profile.</p> */}
                         </div>
-
+                        <p className='description'>Connect to Spotify API, search...{/* songs, create custom playlist, and save playlist to your Spotify profile.*/}</p>
                     </li><br/>   
                     <li>
+                        <h3>Password Generator</h3>
                         <div className='card' id='rpg-card'>
                             <Link className='portfolio-link' id='pwgen' to={{ pathname: "https://sds-smith.github.io/password_generator/" }} target="_blank" >Demo</Link>
                             {/* <span className='divider'>|</span> */}
                             <Link className='portfolio-link rm' id='pwgen-readme' to={{ pathname: "https://github.com/sds-smith/password_generator#readme"}} target='_blank'>About</Link>  
-                            {/* <p className='description'>Generate random unique password and copy to clipboard</p> */}
                         </div>
-
+                         <p className='description'>Generate random unique password and{/* copy to clipboard*/}</p>
                     </li> 
-                </ul> 
-            </div>              
+                </ul>             
         </div>
     );
   }
