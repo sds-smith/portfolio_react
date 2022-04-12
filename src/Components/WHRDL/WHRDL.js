3e6b60ee77e451da5085fc3c8a7fef8a62f4773d
Shawn Smith <sds.smith24@gmail.com>
2022-04-12 13:29:39 -0400

    style and hook up links in WHRDL

--- src/Components/WHRDL/WHRDL.js
+++ src/Components/WHRDL/WHRDL.js
@@ -10,20 +10,24 @@ function WHRDL() {
                 <ul className='WHRDL-list'>
                     <li>
                         <h3>Use iOS Simulator</h3>
-                        <div className='card' id='ios-card' >
-                            <Link className='WHRDL-link' id='ios' to={{ pathname: "https://appetize.io/embed/8bnmakzrptf1hv9dq7v7bnteem?autoplay=false&debug=true&device=iphone12&embed=true&scale=73&screenOnly=false&xDocMsg=true&xdocMsg=true&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelLaunchUrlDefaultsKey%22%3A%22exp%3A%2F%2Fexp.host%2F%40sds-smith%2F037c0d%2Bm6KCUybkh2%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&osVersion=14.5" }} target="_blank" >Demo</Link>
-                            <Link className='WHRDL-link rm' id='atj-readme' to={{ pathname: "https://github.com/sds-smith/assemble-the-jams#readme"}} target='_blank'>About</Link>  
-                        </div>
-                        <p className='description'>Connect to Spotify API, search...{/* songs, create custom playlist, and save playlist to your Spotify profile.*/}</p>
+                        <Link className='card link' id='ios-card' to={{ pathname: "https://appetize.io/embed/8bnmakzrptf1hv9dq7v7bnteem?autoplay=false&debug=true&device=iphone12&embed=true&scale=73&screenOnly=false&xDocMsg=true&xdocMsg=true&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelLaunchUrlDefaultsKey%22%3A%22exp%3A%2F%2Fexp.host%2F%40sds-smith%2F037c0d%2Bm6KCUybkh2%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&osVersion=14.5" }} target="_blank" >
+                        </Link>
+                        <p className='description'>Click phone to run Simulator in your Browser</p>
                     </li><br/>   
                     <li>
                         <h3>Use Android Simulator</h3>
-                        <div className='card' id='rpg-card'>
-                            <Link className='WHRDL-link' id='pwgen' to={{ pathname: "https://sds-smith.github.io/password_generator/" }} target="_blank" >Demo</Link>
-                            {/* <span className='divider'>|</span> */}
-                            <Link className='WHRDL-link rm' id='pwgen-readme' to={{ pathname: "https://github.com/sds-smith/password_generator#readme"}} target='_blank'>About</Link>  
+                        <Link className='card link' id='rpg-card' to={{ pathname: "https://appetize.io/embed/xc1w6f1krd589zhp22a0mgftyw?autoplay=false&debug=true&device=pixel4&embed=true&launchUrl=exp%3A%2F%2Fexp.host%2F%40sds-smith%2F037c0d%2Bm6KCUybkh2&scale=81&screenOnly=false&xDocMsg=true&xdocMsg=true&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelLaunchUrlDefaultsKey%22%3A%22exp%3A%2F%2Fexp.host%2F%40sds-smith%2F037c0d%2Bm6KCUybkh2%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&osVersion=11.0" }} target="_blank">
+                        </Link>
+                         <p className='description'>Click phone to run Simulator in your Browser</p>
+                    </li> 
+                    <li>
+                        <h3>Use your own Device</h3>
+                        <p>(with the Expo Go app)</p>
+                        <div className='card' id='my-device-card' >
                         </div>
-                         <p className='description'>Generate random unique password and{/* copy to clipboard*/}</p>
+                         <p className='description'>iOS : Scan with your Camera App</p>
+                         <p className='description'>Android : Scan with your Expo Go App</p>
+
                     </li> 
                 </ul>             
         </div>
