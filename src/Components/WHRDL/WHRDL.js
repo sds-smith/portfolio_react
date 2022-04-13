416fbeac1d548ac4d64683c88ded2c146012f8cb
Shawn Smith <sds.smith24@gmail.com>
2022-04-13 07:32:25 -0400

    update WHRDL links

--- src/Components/WHRDL/WHRDL.js
+++ src/Components/WHRDL/WHRDL.js
@@ -10,13 +10,13 @@ function WHRDL() {
                 <ul className='WHRDL-list'>
                     <li>
                         <h3>Use iOS Simulator</h3>
-                        <Link className='card link' id='ios-card' to={{ pathname: "https://appetize.io/embed/8bnmakzrptf1hv9dq7v7bnteem?autoplay=true&debug=true&device=iphone12&embed=true&scale=73&screenOnly=false&xDocMsg=true&xdocMsg=true&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelLaunchUrlDefaultsKey%22%3A%22exp%3A%2F%2Fexp.host%2F%40sds-smith%2F037c0d%2Bm6KCUybkh2%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&osVersion=14.5" }} target="_blank" >
+                        <Link className='card link' id='ios-card' to={{ pathname: "https://appetize.io/embed/8bnmakzrptf1hv9dq7v7bnteem?autoplay=true&debug=true&device=iphone12&embed=true&scale=73&screenOnly=false&xDocMsg=true&xdocMsg=true&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelLaunchUrlDefaultsKey%22%3A%22exp%3A%2F%2Fexp.host%2F%40sds-smith%2Fbaad67%2BVSlh7wof24%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&osVersion=14.5" }} target="_blank" >
                         </Link>
                         <p className='description'>Click phone to run Simulator in your Browser</p>
                     </li><br/>   
                     <li>
                         <h3>Use Android Simulator</h3>
-                        <Link className='card link' id='android-card' to={{ pathname: "https://appetize.io/embed/xc1w6f1krd589zhp22a0mgftyw?autoplay=true&debug=true&device=pixel4&embed=true&launchUrl=exp%3A%2F%2Fexp.host%2F%40sds-smith%2F037c0d%2Bm6KCUybkh2&scale=81&screenOnly=false&xDocMsg=true&xdocMsg=true&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelLaunchUrlDefaultsKey%22%3A%22exp%3A%2F%2Fexp.host%2F%40sds-smith%2F037c0d%2Bm6KCUybkh2%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&osVersion=11.0" }} target="_blank">
+                        <Link className='card link' id='android-card' to={{ pathname: "https://appetize.io/embed/xc1w6f1krd589zhp22a0mgftyw?autoplay=true&debug=true&device=pixel4&embed=true&launchUrl=exp%3A%2F%2Fexp.host%2F%40sds-smith%2Fbaad67%2BVSlh7wof24&scale=81&screenOnly=false&xDocMsg=true&xdocMsg=true&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelLaunchUrlDefaultsKey%22%3A%22exp%3A%2F%2Fexp.host%2F%40sds-smith%2Fbaad67%2BVSlh7wof24%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&osVersion=11.0" }} target="_blank">
                         </Link>
                          <p className='description'>Click phone to run Simulator in your Browser</p>
                     </li> 
