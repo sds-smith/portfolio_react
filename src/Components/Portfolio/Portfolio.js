5d686c0ccde0e31c3ea4fdc4b8a9a929257d24c9
Shawn Smith <sds.smith24@gmail.com>
2022-04-17 07:49:27 -0400

    add card array in portfolio and scale 2nd element

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,48 +2,56 @@ import { Link } from 'react-router-dom'
 import './Portfolio.css'
 
 function Portfolio() {
+
+    const cards = [
+        <li>
+            <div className='card-heading'>
+                <h3>Playlist Creator</h3>
+                <p>single-page web app</p>
+            </div>
+
+            <div className='card' id='atj-card'>
+                <Link className='portfolio-link' id='atj' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://assemblethejams.netlify.app" }} target="_blank" >Demo</Link>
+                <Link className='portfolio-link rm' id='atj-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/assemble-the-jams#readme"}} target='_blank'>About</Link>  
+            </div>
+            <p className='description'>Connect to Spotify API, search...{/* songs, create custom playlist, and save playlist to your Spotify profile.*/}</p>
+        </li>,
+
+        <li>
+            <div className='card-heading'>
+                <h3>Word Guess Game</h3>
+                <p>mobile app</p>
+            </div>
+
+            <div className='card' id='WHRDL-card'>
+                <Link className='portfolio-link' id='WHRDL' style={{color: 'rgb(64, 62, 37)'}} to='/WHRDL' >Demo</Link>
+                <Link className='portfolio-link rm' id='WRDL-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/whrdl#readme"}} target='_blank'>About</Link>  
+            </div>
+             <p className='description'>Guess the secret word in six tries.</p>
+        </li>,
+
+        <li>
+            <div className='card-heading'>
+                <h3>Password Generator</h3>
+                <p>single-page web app</p>
+            </div>
+
+            <div className='card' id='rpg-card'>
+                <Link className='portfolio-link' id='pwgen' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://sds-smith.github.io/password_generator/" }} target="_blank" >Demo</Link>
+                <Link className='portfolio-link rm' id='pwgen-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/password_generator#readme"}} target='_blank'>About</Link>  
+            </div>
+             <p className='description'>Generate random unique password and{/* copy to clipboard*/}</p>
+        </li>     
+
+    ]
+
     return (
         <div className='Portfolio' id='page-2'>
                 <h1 id='name'>Shawn Smith</h1>
                 <p className='hr'></p>
                 <h4 id='role'>portfolio samples</h4>
                 <ul className='portfolio-list'>
-                    <li>
-                        <div className='card-heading'>
-                            <h3>Playlist Creator</h3>
-                            <p>single-page web app</p>
-                        </div>
-
-                        <div className='card' id='atj-card'>
-                            <Link className='portfolio-link' id='atj' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://assemblethejams.netlify.app" }} target="_blank" >Demo</Link>
-                            <Link className='portfolio-link rm' id='atj-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/assemble-the-jams#readme"}} target='_blank'>About</Link>  
-                        </div>
-                        <p className='description'>Connect to Spotify API, search...{/* songs, create custom playlist, and save playlist to your Spotify profile.*/}</p>
-                    </li><br/>   
-                    <li>
-                        <div className='card-heading'>
-                            <h3>Word Guess Game</h3>
-                            <p>mobile app</p>
-                        </div>
-
-                        <div className='card' id='WHRDL-card'>
-                            <Link className='portfolio-link' id='WHRDL' style={{color: 'rgb(64, 62, 37)'}} to='/WHRDL' >Demo</Link>
-                            <Link className='portfolio-link rm' id='WRDL-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/whrdl#readme"}} target='_blank'>About</Link>  
-                        </div>
-                         <p className='description'>Guess the secret word in six tries.</p>
-                    </li> 
-                    <li>
-                        <div className='card-heading'>
-                            <h3>Password Generator</h3>
-                            <p>single-page web app</p>
-                        </div>
-
-                        <div className='card' id='rpg-card'>
-                            <Link className='portfolio-link' id='pwgen' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://sds-smith.github.io/password_generator/" }} target="_blank" >Demo</Link>
-                            <Link className='portfolio-link rm' id='pwgen-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/password_generator#readme"}} target='_blank'>About</Link>  
-                        </div>
-                         <p className='description'>Generate random unique password and{/* copy to clipboard*/}</p>
-                    </li> 
+                    {cards}
                 </ul>             
         </div>
     );
