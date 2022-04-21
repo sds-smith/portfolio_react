8cdae8dfa95b3b4b246052f9d88e8c82fcb6bff7
Shawn Smith <sds.smith24@gmail.com>
2022-04-21 10:59:52 -0400

    add responsive styling in about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,25 +6,39 @@ import Resume from '../../media/Shawn_Smith_resume_04202022.png'
 function About() {
 
     const [skills, setSkills] = useState([
-                    ['li', 'Responsive Web Design with HTML5 and CSS3'], 
-                    ['li', 'Interactive Web Development with Javascript'], 
-                    ['li', 'Web APIs and Asynchronous Request Handling'],
-                    ['li', 'Javascript Algorithms and Data Structures'],
-                    ['li', 'Object-oriented and Functional Programming'],
-                    ['li', 'Single-page Web Apps with React.js'],
-                    ['li', 'Mobile Apps with React Native'],
-                    ['li', 'Git and GitHub'],
-                    ['li', 'Web Accessibility'],
-                    ['li', 'oAuth 2.0 Authentication'],
-                    ['li', 'Search Engine Optimization']
+                    'Responsive Web Design with HTML5 and CSS3', 
+                    'Interactive Web Development with Javascript', 
+                    'Web APIs and Asynchronous Request Handling',
+                    'Javascript Algorithms and Data Structures',
+                    'Object-oriented and Functional Programming',
+                    'Single-page Web Apps with React.js',
+                    'Mobile Apps with React Native',
+                    'Git and GitHub',
+                    'Web Accessibility',
+                    'oAuth 2.0 Authentication',
+                    'Search Engine Optimization'
                     ])
-        
+    const [scrollerInterval, setScrollerInterval] = useState(2000)
+
+
+    
     useEffect(() => {
         const interval = setInterval(() => {
-          setSkills([...skills.slice(1), skills[0]])
-        }, 2000);
+            setSkills([...skills.slice(1), skills[0]])
+          }, scrollerInterval);
         return () => clearInterval(interval);
-    }, [skills]); 
+    }, [scrollerInterval, skills]); 
+
+ 
+    useEffect(() => {
+        if (scrollerInterval % 10 === 0) {
+            setSkills(skills => [...skills.slice(1), skills[0]])
+        }
+    },[scrollerInterval])
+
+    const slideAction = (e) => {
+        setScrollerInterval(e.target.value)
+    }
 
     return (
         <div className='About' id='page-3'>
@@ -45,20 +59,24 @@ function About() {
                     <p >After working most of my adult life in the restaurant industry, in 2021 I decided a career change was in order, and with the support of my wife and family, I set myself down the path toward a career in Web and Mobile Development.</p>
                     <p>From my experience as a multi-unit restaurant leader, I bring soft skills like strong communication, collaboration, relationship-building, partnering and mentoring.  I bring over a decade of experience iterating, prototyping, and scaling an ever-changing operational model.</p>    
                     <p>From my ongoing studies in Web and Mobile Development, I have attained valuable technical skills, including:</p>    
+                    <div className='skills-scroller'>
+                        <input type='range' className='slider' min={50} max={2000} step={1} value={scrollerInterval} onChange={slideAction} ></input>
                         <ul>
-                            <li>{skills[0][1]}</li>
-                            <li>{skills[1][1]}</li>
-                            <li>{skills[2][1]}</li>
-                            <li>{skills[3][1]}</li>
-                            <li>{skills[4][1]}</li>
-                            <li>{skills[5][1]}</li>
-                            <li>{skills[6][1]}</li>
+                            <li>{skills[0]}</li>
+                            <li>{skills[1]}</li>
+                            <li>{skills[2]}</li>
+                            <li>{skills[3]}</li>
+                            <li>{skills[4]}</li>
+                            <li>{skills[5]}</li>
+                            <li>{skills[6]}</li>
                         </ul>
+                    </div>
+
                 <p>I recently attained a score of 725 on the General Coding Assessment administered by CodeSignal (815 on the practice test), and completed the Front End Engineer certification from Codecademy.
-                        I am driven by the desire to be better today than yesterday, better tomorrow than today. I love to solve problems and I thrive in a fast-paced work environment.  
+                        I am driven by the desire to be better today than yesterday, better tomorrow than today, and by the belief that the moment we stop learning, we stop living. I love to solve problems and I thrive in a fast-paced work environment.  
                     </p>
 
-                    <img src={Resume} className='Resume'/>
+                    <img src={Resume} className='Resume' alt='Shawn Smith Resume'/>
                 </div>
             </div>               
         </div>
