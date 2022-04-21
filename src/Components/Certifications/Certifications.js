c2420dde5bfa744bd9d1b801602344f49164f243
Shawn Smith <sds.smith24@gmail.com>
2022-04-21 13:36:00 -0400

    add photo files and imports for certifications

--- src/Components/Certifications/Certifications.js
+++ src/Components/Certifications/Certifications.js
@@ -1,6 +1,14 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom'
+
 import './Certifications.css'
+import Command_Line from '../../media/certifications/Command_Line_Codecademy.png'
+import Front_End from '../../media/certifications/Front_End_Codecademy.png'
+import HTML from '../../media/certifications/HTML_Codecademy.png'
+import Intermediate_Javascript from '../../media/certifications/Intermediate_Javascript_Codecademy.png'
+import Javascript from '../../media/certifications/Javascript_Codecademy.png'
+import React from '../../media/certifications/React_Codecademy.png'
+import React_Native from '../../media/certifications/React_Native_Codecademy.png'
+import Vue from '../../media/certifications/Vue_Codecademy.png'
+
 
 function Certifications() {
 
@@ -10,7 +18,7 @@ function Certifications() {
                 <p className='hr'></p>
                 <h4 id='role'>certifications</h4>
                 <div className='cert-container'>
-                    
+                    <img src={} alt=''/>
                 </div>
         </div>
     )
