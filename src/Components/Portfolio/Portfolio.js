1abee60aba4975c80e188f644fb0e76791aabfe2
Shawn Smith <sds.smith24@gmail.com>
2022-04-30 17:14:29 -0400

    update portfolio descriptions

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -7,8 +7,8 @@ function Portfolio() {
     const [cards, setCards] = useState([
         <li>
             <div className='card-heading'>
-                <h3>Playlist Creator</h3>
-                <p>single-page web app</p>
+                <h3>Assemble the Jams</h3>
+                <p>React single-page web app</p>
             </div>
 
             <div className='card' id='atj-card'>
@@ -19,8 +19,8 @@ function Portfolio() {
 
         <li>
             <div className='card-heading'>
-                <h3>Word Guess Game</h3>
-                <p>mobile app</p>
+                <h3>WHRDL</h3>
+                <p>React Native mobile app</p>
             </div>
 
             <div className='card' id='WHRDL-card'>
@@ -32,7 +32,7 @@ function Portfolio() {
         <li>
             <div className='card-heading'>
                 <h3>Password Generator</h3>
-                <p>single-page web app</p>
+                <p>HTML/JS single-page web app</p>
             </div>
 
             <div className='card' id='rpg-card'>
@@ -44,8 +44,8 @@ function Portfolio() {
     ])
 
     const [descriptions, setDescriptions] = useState([
-        'Connect to Spotify API, search songs, create custom playlist, and save playlist to your Spotify profile.',
-        'Guess the secret word in six tries.',
+        'Connect to your Spotify Premium account, execute searches and receive search term matches and recommendations, play/pause a song, like/ unlike songs, create custom playlist, and save playlist to your Spotify profile.',
+        'Clone of a popular word game. Guess the secret word in six tries. Reset game, get hints',
         'Generate random unique password and copy to clipboard'
     ])
 
