76ad5531f4c39f130674a91ee5c237cfad36f706
Shawn Smith <sds.smith24@gmail.com>
2022-04-30 19:20:29 -0400

    style contact bar

--- src/Components/ContactBar/ContactBar.js
+++ src/Components/ContactBar/ContactBar.js
@@ -19,10 +19,13 @@ function ContactBar(props) {
     return (
         <div className='contact-info' id='contact'>
             <Link to='/contact_form' className='side-nav' onClick={toggleContact}>contact:</Link>
-            <h5><a className='contact-link'href='mailto:sds.smith24@gmail.com' id='email'   onClick={toggleContact}><span>sds.smith24@gmail.com</span><img className='icon' src={email_icon} alt='email icon for sds.smith24@gmail.com'/></a></h5>
-            <h5><a className='contact-link'href='https://www.linkedin.com/in/shawn-smith-3379ab13/' target='_blank'  rel="noreferrer" onClick={toggleContact}><span>linkedin.com/shawn-smith</span><img className='icon' src={linkedin_icon} alt='linkedin icon for linkedin.com/shawn-smith'/></a></h5>
-            <h5><a className='contact-link'href='https://github.com/sds-smith' target='_blank' rel="noreferrer" onClick={toggleContact}><span>github.com/sds-smith</span><img className='icon' src={github_icon} alt='github icon for github.com/sds-smith'/></a></h5>
-            <h5><a className='contact-link'href='tel:12156305599'><span>215.630.5599</span><img className='icon' src={phone_icon} alt='phone icon for 215.630.5599'/></a></h5>                
+            <div className='icon_container'>
+                <h5><a className='contact-link'href='mailto:sds.smith24@gmail.com' id='email'   onClick={toggleContact}><img className='icon' src={email_icon} alt='email icon for sds.smith24@gmail.com'/></a></h5>
+                <h5><a className='contact-link'href='https://www.linkedin.com/in/shawn-smith-3379ab13/' target='_blank'  rel="noreferrer" onClick={toggleContact}><img className='icon' src={linkedin_icon} alt='linkedin icon for linkedin.com/shawn-smith'/></a></h5>
+                <h5><a className='contact-link'href='https://github.com/sds-smith' target='_blank' rel="noreferrer" onClick={toggleContact}><img className='icon' src={github_icon} alt='github icon for github.com/sds-smith'/></a></h5>
+                <h5><a className='contact-link'href='tel:12156305599'><img className='icon' src={phone_icon} alt='phone icon for 215.630.5599'/></a></h5>    
+            </div>
+            
         </div>
     );
   }
