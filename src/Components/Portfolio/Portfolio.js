3f95238878768ed09a5bd766c4967ddbfaf03ccc
Shawn Smith <sds.smith24@gmail.com>
2022-06-14 13:06:25 -0400

    convert Portfolio to scss, map cards array

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,91 +1,18 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
-import './Portfolio.css'
+import './Portfolio.scss'
+import { Projects } from '../../data/projects'
 
 function Portfolio() {
 
-    const [cards, setCards] = useState([
-        <li>
-            <div className='card-heading'>
-                <h3>Assemble the Jams</h3>
-                <p>React single-page web app</p>
-            </div>
-
-            <div className='card' id='atj-card'>
-                <Link className='portfolio-link rm' id='atj-video' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://youtu.be/fMgUQI0HRTk"}} target='_blank'>View Demo</Link>  
-                <Link className='portfolio-link' id='atj' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://assemblethejams.netlify.app" }} target="_blank" >Open App</Link>
-                <Link className='portfolio-link rm' id='atj-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/assemble-the-jams/blob/main/README.md"}} target='_blank'>More Info</Link>  
-           
-            </div>
-        </li>,
-
-        <li>
-            <div className='card-heading'>
-                <h3>Crown Clothing</h3>
-                <p>React single-page eCommerce app</p>
-            </div>
-
-            <div className='card' id='crwn-card'>
-                <Link className='portfolio-link' id='crwn-clothing' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://sds-crown-clothing.netlify.app" }} target="_blank" >Open App</Link>
-                <Link className='portfolio-link rm' id='crwn-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/crwn-clothing/blob/master/README.md"}} target='_blank'>More Info</Link>  
-        
-            </div>
-        </li>,
-
-        <li>
-            <div className='card-heading'>
-                <h3>WHRDL</h3>
-                <p>React Native mobile app</p>
-            </div>
-
-            <div className='card' id='WHRDL-card'>
-                <Link className='portfolio-link' id='WHRDL' style={{color: 'rgb(64, 62, 37)'}} to='/WHRDL' >Open App</Link>
-                <Link className='portfolio-link rm' id='WRDL-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/whrdl/blob/master/README.md"}} target='_blank'>More Info</Link>  
-            </div>
-        </li>,
-
-        <li>
-            <div className='card-heading'>
-                <h3>Password Generator</h3>
-                <p>HTML/JS single-page web app</p>
-            </div>
-
-            <div className='card' id='rpg-card'>
-                <Link className='portfolio-link' id='pwgen' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://sds-smith.github.io/password_generator/" }} target="_blank" >Open App</Link>
-                <Link className='portfolio-link rm' id='pwgen-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/password_generator/blob/master/README.md"}} target='_blank'>More Info</Link>  
-            </div>
-        </li>,
-
-        <li>
-            <div className='card-heading'>
-                <h3>Weather App</h3>
-                <p>Vue single-page web app</p>
-            </div>
-
-            <div className='card' id='weather-card'>
-                <Link className='portfolio-link' id='weather' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: "https://sds-smith.github.io/weather/" }} target="_blank" >Open App</Link>
-                <Link className='portfolio-link rm' id='weather-readme' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: "https://github.com/sds-smith/weather/blob/master/README.md"}} target='_blank'>More Info</Link>  
-            </div>
-        </li>  
-    ])
-
-    const [descriptions, setDescriptions] = useState([
-        'Connect to your Spotify Premium account, execute searches and receive search term matches and recommendations, play/pause a song, like/ unlike songs, create custom playlist, and save playlist to your Spotify profile.',
-        'Fully-functional eCommerce site built with React 18, React Router 6, Firebase. Securely sign-in with google, or create and account to sign in with username/password. Add items to cart, delete items, securely checkout with Stripe.',
-        'Clone of a popular word game. Guess the secret word in six tries. Reset game, get hints',
-        'Generate random unique password and copy to clipboard',
-        'Search by City/ State/ Country, or by longitudinal coordinates, and receive up-to-the-minute weather data'
-    ])
+    const [cards, setCards] = useState(Projects)
 
     const advanceCarousel = () => {
         setCards([cards[cards.length-1], ...cards.slice(0, cards.length-1)])
-        setDescriptions([descriptions[descriptions.length-1], ...descriptions.slice(0, descriptions.length-1)])
     }
 
     const reverseCarousel = () => {
         setCards([...cards.slice(1), cards[0]])
-        setDescriptions([...descriptions.slice(1), descriptions[0]])
-
     }
 
     // useEffect(() => {
@@ -100,11 +27,28 @@ function Portfolio() {
                 <div className='carousel'>
                     <button onClick={advanceCarousel}>&#10150;</button>  {/*&#10149*/}         
                     <ul className='portfolio-list'>
-                        {cards.slice(0, 3)}
+                        { cards.slice(0, 5).map((card) => {
+                            return (
+                                <li key={card.id}>
+                                    <div className='card-heading'>
+                                        <h3>{card.heading_title}</h3>
+                                        <p>{card.heading_subtitle}</p>
+                                    </div>
+                                
+                                    <div className='card' id={card.id}>
+                                        { card.video_link && <Link className='portfolio-link rm' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: card.video_link}} target='_blank'>View Demo</Link>}  
+                                        <Link className='portfolio-link' style={{color: 'rgb(64, 62, 37)'}} to={{ pathname: card.live_link }} target="_blank" >Open App</Link>
+                                        <Link className='portfolio-link rm' style={{color: 'rgb(217, 213, 169)'}} to={{ pathname: card.readme_link}} target='_blank'>More Info</Link>  
+                                    </div>
+                                </li>
+                            )
+                        })
+                        }
+
                     </ul>  
                     <button className='reverse-btn' onClick={reverseCarousel}>&#10150;</button>           
                 </div>
-                <h2 className='description'>{descriptions[1]}</h2>
+                <h2 className='description'>{cards[2].description}</h2>
         </div>
     );
   }
